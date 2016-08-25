63ee3addff022599ff4d785637883b795904fa87
edwinJJ <edwinj0326@gmail.com>
2016-08-25 10:10:34 +0900

    8/25(목) - 1차

--- Wholegram/src/main/java/net/nigne/wholegram/domain/MemberVO.java
+++ Wholegram/src/main/java/net/nigne/wholegram/domain/MemberVO.java
@@ -11,7 +11,6 @@ public class MemberVO {
 	private String email;
 	private String phone;
 	private String gender;
-	private int recommend;
 	private MultipartFile user_profile;
 	private int default_profile;
 	private int follow;
@@ -71,14 +70,6 @@ public class MemberVO {
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-	
-	public int getRecommend() {
-		return recommend;
-	}
-	public void setRecommend(int recommend) {
-		this.recommend = recommend;
-	}
-	
 	public MultipartFile getUser_profile() {
 		return user_profile;
 	}
